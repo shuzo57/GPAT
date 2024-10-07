ede4bfd8f99540482442b9829c32ffdc6ca4f0e1
kitano <kitano>
2024-10-07 10:15:24 +0900

    サブコマンドの削除

--- gpat/__main__.py
+++ gpat/__main__.py
@@ -31,74 +31,40 @@ def main():
     args_parser.add_argument("-dc", "--det-config", type=str, default=det_checkpoint, help="detection config path")
     args_parser.add_argument("-f", "--fpat", action="store_true", default=False, help="convert GPAT data to FPAT data")
     args_parser.add_argument("-s", "--save_img", action="store_true", default=False, help="save images")
-    
-    # サブコマンドを追加するためのパーサを作成
-    subparsers = args_parser.add_subparsers(dest="command")
 
-    # 'first'コマンドの設定
-    first_parser = subparsers.add_parser("first", help="human detection and tracking")
-    first_parser.add_argument("-i", "--input", type=str, required=True, help="input video file path")
-    first_parser.add_argument("-o", "--output", type=str, default=os.getcwd(), help="output directory path")
-    first_parser.add_argument("-m", "--det-model", type=str, default=det_model, help="detection model path")
-    first_parser.add_argument("-c", "--det-config", type=str, default=det_checkpoint, help="detection config path")
-    
-    # 'second'コマンドの設定
-    second_parser = subparsers.add_parser("second", help="pose estimation from tracking data")
-    second_parser.add_argument("-i", "--input", type=str, required=True, help="input video file path")
-    second_parser.add_argument("-t", "--tracking_data", type=str, required=True, help="tracking data file path")
-    second_parser.add_argument("-o", "--output", type=str, default=os.getcwd(), help="output directory path")
-    second_parser.add_argument("-m", "--pose-model", type=str, default=pose_model, help="pose model path")
-    second_parser.add_argument("-c", "--pose-config", type=str, default=pose_checkpoint, help="pose config path")
-    
     # 引数を解析
     args = args_parser.parse_args()
-    if args.command == "first":
-        detect_and_track(
+    if os.path.isfile(args.input):
+        pose_estimate_and_track(
             video_path=args.input,
             output_path=args.output,
-            model_path=args.det_model,
-            config_path=args.det_config
+            pose_model=args.pose_model,
+            pose_checkpoint=args.pose_config,
+            det_model=args.det_model,
+            det_checkpoint=args.det_config,
+            save_img=args.save_img
         )
-    elif args.command == "second":
-        pose_estimate_from_tracking_data(
-            video_path=args.input,
-            tracking_data_path=args.tracking_data,
-            model_path=args.pose_model,
-            config_path=args.pose_config,
-            output_path=args.output
-        )
-    else:
-        if os.path.isfile(args.input):
-            pose_estimate_and_track(
-                video_path=args.input,
-                output_path=args.output,
-                pose_model=args.pose_model,
-                pose_checkpoint=args.pose_config,
-                det_model=args.det_model,
-                det_checkpoint=args.det_config,
-                save_img=args.save_img
-            )
-            video_name = os.path.basename(args.input).split('.')[0]
-            if args.fpat:
-                gpat2fpat(os.path.join(args.output, "data", video_name))
-        elif os.path.isdir(args.input):
-            for file_ in os.listdir(args.input):
-                ex = file_.split('.')[-1]
-                if os.path.isdir(os.path.join(args.input, file_)):
-                    continue  # ignore directory
-                if ex in MEDIA_EXTENSIONS:
-                    pose_estimate_and_track(
-                        video_path=os.path.join(args.input, file_),
-                        output_path=args.output,
-                        pose_model=args.pose_model,
-                        pose_checkpoint=args.pose_config,
-                        det_model=args.det_model,
-                        det_checkpoint=args.det_config,
-                        save_img=args.save_img
-                    )
-                    video_name = os.path.basename(file_).split('.')[0]
-                    if args.fpat:
-                        gpat2fpat(os.path.join(args.output, "data", video_name))
+        video_name = os.path.basename(args.input).split('.')[0]
+        if args.fpat:
+            gpat2fpat(os.path.join(args.output, "data", video_name))
+    elif os.path.isdir(args.input):
+        for file_ in os.listdir(args.input):
+            ex = file_.split('.')[-1]
+            if os.path.isdir(os.path.join(args.input, file_)):
+                continue  # ignore directory
+            if ex in MEDIA_EXTENSIONS:
+                pose_estimate_and_track(
+                    video_path=os.path.join(args.input, file_),
+                    output_path=args.output,
+                    pose_model=args.pose_model,
+                    pose_checkpoint=args.pose_config,
+                    det_model=args.det_model,
+                    det_checkpoint=args.det_config,
+                    save_img=args.save_img
+                )
+                video_name = os.path.basename(file_).split('.')[0]
+                if args.fpat:
+                    gpat2fpat(os.path.join(args.output, "data", video_name))
 
 if __name__ == "__main__":
     main()
